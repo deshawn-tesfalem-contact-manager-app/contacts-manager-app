5eac56ec40c70b5bd13d2427c4a93bd692fcd83e
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-10-27 09:40:04 -0500

    finished Contact() object class

--- src/contact_manager/Contact.java
+++ src/contact_manager/Contact.java
@@ -2,4 +2,25 @@ package contact_manager;
 
 public class Contact {
 
+    private String name;
+    private String number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void editName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void editNumber(String number) {
+        this.number = number;
+    }
+    public String getInfo(){
+        return (String.format("%s, %s", name, number));
+    }
 }
