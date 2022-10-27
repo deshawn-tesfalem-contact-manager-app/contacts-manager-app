662235f733d19a85697fe66d521fcf3db10ecfa2
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-10-27 13:14:31 -0500

    Contact manager tester improvisions

--- src/contact_manager/ContactManagerTester.java
+++ src/contact_manager/ContactManagerTester.java
@@ -3,5 +3,15 @@ package contact_manager;
 public class ContactManagerTester {
     public static void main(String [] args){
         ContactManager cm = new ContactManager();
+
+        //test if there are no contacts in the array
+        cm.getContacts();
+        //retrieves contacts from contact file
+        cm.contactRetriever();
+        //test if contacts were added
+        cm.getContacts();
+
     }
+
+
 }
